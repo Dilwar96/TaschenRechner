4161d3ae4b7b82d166aeb3011a532b165d802430
Dilwar96 <157256888+Dilwar96@users.noreply.github.com>
2024-04-13 11:18:22 +0200

    Refactor: replace target with currentTarget

--- src/Components/TaschenRechner.tsx
+++ src/Components/TaschenRechner.tsx
@@ -20,90 +20,90 @@ const TaschenRechner = () => {
             <input
               type="button"
               value="."
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="/"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
           </div>
           <div>
             <input
               type="button"
               value="7"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="8"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="9"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="*"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
           </div>
           <div>
             <input
               type="button"
               value="4"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="5"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="6"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="+"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
           </div>
           <div>
             <input
               type="button"
               value="1"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="2"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="3"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="-"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
           </div>
           <div>
             <input
               type="button"
               value="00"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
               value="0"
-              onClick={(e) => setValue(value + e.target.value)}
+              onClick={(e) => setValue(value + e.currentTarget.value)}
             />
             <input
               type="button"
